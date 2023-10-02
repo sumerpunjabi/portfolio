29a2e057382229d7f52bb8c63cbe8ca2f765784e
Sumer Punjabi <sumer@carwale.com>
2023-10-01 22:15:45 -0400

    created react project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import './App.module.scss';
-import BaseLayout from "./components/BaseLayout";
-import {BrowserRouter} from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-   return (
-      <div>
-         <BrowserRouter>
-            <BaseLayout/>
-         </BrowserRouter>
-      </div>
-   );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-
 
 export default App;
