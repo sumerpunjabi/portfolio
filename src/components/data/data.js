e72c53b5233a18c9a31b88da00be5b8dd48a7147
Sumer Punjabi <sumer@carwale.com>
2023-10-04 19:36:44 -0400

    changed to vite project and built basics

---
+++ src/components/data/data.js
@@ -0,0 +1,30 @@
+export const projects = [
+    {
+      name: "Olympics REST API",
+      projects: "Serve historical data from the Olympics",
+      year: "2023",
+      url: 'https://github.com/sumerpunjabi/olympics-api',
+      keywords: ["API", "Python", "MySQL"],
+    },
+    {
+      name: "Directory Encryption",
+      projects: "Service to protect your files on startup and shutdown",
+      year: "2023",
+      url: 'https://github.com/caumaria/diary',
+      keywords: ["AES", "RSA", "Cryptography", "Linux"],
+    },
+    {
+      name: "AODA App",
+      projects: "Flutter app to detect objects for the impaired",
+      year: "2023",
+      url: 'https://github.com/sumerpunjabi',
+      keywords: ["Flutter", "Dart", "TensorFlow"],
+    },
+    {
+      name: "Meme Delivery Bot",
+      projects: "Post to Instagram using Meta Graph API",
+      year: "2021",
+      url: 'https://github.com/sumerpunjabi/MemeDeliveryBot',
+      keywords: ["Python", "Scripting", "API"],
+    }
+];
